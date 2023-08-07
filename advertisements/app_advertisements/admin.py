dd269720457abbc4cd5b95e795588ff6bf06db31
Tim <timch201008@gmail.com>
2023-08-07 21:51:56 +0300

    4 version

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Advertisement
+from django.db.models.query import QuerySet
+
+# класс для кастомизации модели в админке
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'description', 'auction', 'created_at', 'created_date', 'update_date'] # столбцы для отображения в таблице
+    list_filter = ['auction', 'created_at', 'price'] # столбцы по которым будет фильтрация
+    actions = ['make_action_as_false', 'make_action_as_true'] # методы для выбранных записей
+    fieldsets = (
+        ('Общие', {
+            "fields":(
+                'title', 'description'
+            ),
+        }),
+        ('Финансы', {
+            "fields": (
+                'price', 'auction'
+            ),
+            'classes': ['collapse']
+        }),
+    )
+
+
+    @admin.action(description="Убрать возможность торга")
+    def make_action_as_false(self, request, queryset:QuerySet):
+        queryset.update(auction = False) # обновить значение аукцион у выбранных записей на False
+
+    @admin.action(description="Добавить возможность торга")
+    def make_action_as_true(self, request, queryset: QuerySet):
+        queryset.update(auction = True)
+
+
+
+# подключение модели в админку
+admin.site.register(Advertisement, AdvertisementAdmin)
 
 # Register your models here.
+
+
+def dec(func):
+    def wrapper():
+        print()
+        func()
+        print()
+
+    return wrapper()
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+from django.contrib import admin
+from django.utils import timezone # для времени
+from django.utils.html import format_html # для создания строки html
+
+
 # Create your models here.
+
 
 
 
@@ -11,6 +17,27 @@ class Advertisement(models.Model):
     auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # если запись создана сегодня то отображение зеленым если нет то серым
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():  # проверяем что запись была создана сегодня
+            created_time = self.created_at.time().strftime('%H.%M.%S')
+            return format_html(
+                "<span style='color:green; font-weight: bold'>Сегодня в {}</span>",
+                created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y at %H.%M.%S')
+
+    @admin.display(description='дата обновления')
+    def update_date(self):
+        if self.updated_at.date() == timezone.now().date():  # проверяем что запись была создана сегодня
+            created_time = self.updated_at.time().strftime('%H.%M.%S')
+            return format_html(
+                "<span style='color:green; font-weight: bold'>Сегодня в {}</span>",
+                created_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y at %H.%M.%S')
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price}) "
